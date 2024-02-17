2bf848f84ebfeddcf51483c36c33f74810783b7d
Sohil Harrybarran <159188014+Sohilh08@users.noreply.github.com>
2024-02-17 13:01:23 +0200

    Update script.js

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -1,1 +1,24 @@
+// STEP 1: Wrap the entire contents of script.js inside of an IIFE
+(function () {
 
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // STEP 10: Loop over the names array and say either 'Hello' or "Good Bye"
+  // using either the helloSpeaker's or byeSpeaker's 'speak' method.
+  for (var i = 0; i < names.length; i++) {
+
+    // STEP 11: Retrieve the first letter of the current name in the loop.
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // STEP 12: Compare the 'firstLetter' retrieved in STEP 11 to lower case
+    // 'j'. If the same, call byeSpeaker's 'speak' method with the current name
+    // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
+    // name in the loop.
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})();
